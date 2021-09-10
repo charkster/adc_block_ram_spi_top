b22f350cef50b6f4713b881f7d3c9a840b5d7bf1
charkster <31560568+charkster@users.noreply.github.com>
2021-09-09 22:26:23 -0700

    Add files via upload

--- rtl/block_ram.sv
+++ rtl/block_ram.sv
@@ -12,7 +12,10 @@ module block_ram_dual_port
 
    (* RAM_STYLE="BLOCK" *)
    logic [RAM_WIDTH-1:0] bram [(2**RAM_ADDR_BITS)-1:0];
-
+   
+   initial
+     $readmemh("bram_initial_hex_values.data", bram, 0, (2**RAM_ADDR_BITS)-1);
+   
    always@(posedge clk) begin
      if (write_enable) bram[write_address] <= write_data;
      if (read_enable)            read_data <= bram[read_address];
